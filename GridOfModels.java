3cf202402f1933baee72e97fddb677d2256b3451
Laith Abad <laithabad@gmail.com>
2019-12-02 19:18:36 -0600

    Create
    yep

---
+++ GridOfModels.java
@@ -0,0 +1,25 @@
+package com.example.laithabad.tareigntest;
+
+/**
+ * Created by laithabad on 11/20/2019.
+ */
+
+public class GridOfModels {
+    private Integer[] grid;
+
+
+    public GridOfModels(Integer[] grid) {
+        this.grid = grid;
+    }
+
+    public Integer[] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Integer[] grid) {
+        this.grid = grid;
+    }
+
+
+
+}
